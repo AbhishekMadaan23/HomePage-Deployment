689743c3221d0ac3aaa5e4c12c597fba57f46f88
AbhishekMadaan23 <104679188+AbhishekMadaan23@users.noreply.github.com>
2024-02-05 18:05:00 +0530

    Create banner comp

---
+++ src/Components/banner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Container = styled.div``;
+
+const banner = () => {
+  return (
+    <div>
+      <Container>
+        <h1 style={{ backgroundColor: "red" }}>WELCOME TO THE BLOG</h1>
+      </Container>
+    </div>
+  );
+};
+
+export default banner;
